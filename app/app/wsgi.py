85a33df4fd68d22607eaee9a52921ecd1794a962
Evgenslam <evgenyb86@gmail.com>
2024-04-24 16:20:00 +0700

    Alter wsgi file to enable running  gunicorn

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+# Altered from os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings') dut to docker container structure
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.app.settings')
 
 application = get_wsgi_application()
